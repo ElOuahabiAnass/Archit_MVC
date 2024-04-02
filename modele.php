ce293116e78755446c03669a2c53097df5c79cc9
ElOuahabiAnass <154347714+ElOuahabiAnass@users.noreply.github.com>
2024-04-02 15:35:27 +0000

    Create modele.php

---
+++ modele.php
@@ -0,0 +1,13 @@
+// Modele.php
+class Modele {
+    private $bdd;
+
+    function __construct() {
+        $this->bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'root', '');
+    }
+
+    public function getBillets() {
+        $billets = $this->bdd->query('SELECT BIL_ID AS id, BIL_DATE AS date, BIL_TITRE AS titre, BIL_CONTENU AS contenu FROM T_BILLET ORDER BY BIL_ID DESC');
+        return $billets;
+    }
+}
